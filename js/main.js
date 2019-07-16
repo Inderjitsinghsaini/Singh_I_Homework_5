5aa68165829008c706579843ac2a803ffd1f1690
Singh <i_singh94252@tss-hr420-dm04.fanshawec.ca>
2019-07-16 13:56:53 -0400

    done with the responsive navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 (() => {
 	 console.log("fired")
+
+	 let mobileNav = document.querySelector('.nav'),
+	 navPanel =document.querySelector('#burgerCon'),
+	 navLinks = navPanel.querySelector('a');
+	 function toggleNav(event){
+	 	event.preventDefault();
+
+
+	 	console.log('should show nav dropdown');
+	 
+	 	if (this.nodeName == "A"){
+	 		debugger;
+	 		//we clicked on an anchor tag, so do some navigation
+	 		
+	 		//window.scrollTo({
+	 			//top: this.offsetTop,
+	 			//behavior: 'smooth'
+	 		//})
+	 	//}
+	 	}
+	 	navPanel.classList.toggle('show-mobile-nav');
+	 }
+
+	 mobileNav.addEventListener('click', toggleNav);
+	 navLinks.forEach(link => link.addEventListener('click', toggleNav));
+
+
+
+
+
+
+
 })();
